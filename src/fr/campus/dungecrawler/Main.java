1dcf28c8d41d3b99216f02db9a067ddf3f1b5bb7
aliyousfi-art <ali.yousfi@le-campus-numerique.fr>
2026-03-11 10:22:19 +0100

    mise a jour

--- src/fr/campus/dungecrawler/Main.java
+++ src/fr/campus/dungecrawler/Main.java
@@ -7,6 +7,8 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Welcome to the game of dungeoncrawler");
+
         Menu menu = new Menu();
         Game game = new Game(menu);
 
